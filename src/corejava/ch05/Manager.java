1dc67dd384abb64b86102746ff58aca6bbadc37f
281034503 <281034503@qq.com>
2017-12-21 17:54:13 +0800

    继承Employee,计算薪水

--- src/corejava/ch05/Manager.java
+++ src/corejava/ch05/Manager.java
@@ -1,5 +1,20 @@
 package corejava.ch05;
 
-public class Manager {
+public class Manager extends Employee{
+	private double bonus;
+
+	public Manager(String name, double salary, int year, int month, int day) {
+		super(name, salary, year, month, day);
+		bonus=0;
+	}
+	public double getSalary() {
+		double baseSalary=super.getSalary();
+		return baseSalary+bonus;
+	}
+	
+	public void SetBonus(double b) {
+		bonus=b;
+	}
+	
 
 }
