6d0f3ee6dedc9a664f1f17cfd223ee3ee484d00a
281034503 <281034503@qq.com>
2018-01-31 11:51:22 +0800

    显示一个窗体捕获异常

--- src/corejava/ch07/except/ExceptTest.java
+++ src/corejava/ch07/except/ExceptTest.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 import javax.swing.*;
-
+   
 public class ExceptTest {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
